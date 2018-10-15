c3b3562d324944c319b597d08bcf5321315db7d0
Eric <eli20@choate.edu>
2018-10-15 11:13:51 -0400

    Red Pixels
    The Red Pixels

--- redpic.py
+++ redpic.py
@@ -11,18 +11,24 @@ image = Image.new("RGB",(imgx,imgy))
 
 red = 255
 reference = 0
+color2 = 0
+color3 = 0
 
 for x in range(512):
 	for y in range(512):
-		image.putpixel((x,y),(red,0,0))
-		if red == 255 or red == 0:
+		image.putpixel((x,y),(red,color2,color3))
+		if red == 255 or red == 0 or color2 == 255 or color2 == 0 or color3 == 255 or color3 ==0:
 			reference += 1
 
 		if reference % 2 == 1:
 			red -= 3
+			color2 += 3
+			color3 += 3
 
 		else:
 			red += 3
+			color2 -= 3
+			color3 -= 3
 
 image.save("image.png", "PNG")
 
