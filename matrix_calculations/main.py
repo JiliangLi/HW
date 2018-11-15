7ad476279a9d63594b85cae14058031de77be8db
Eric <eli20@choate.edu>
2018-11-15 11:15:06 -0500

    matrix calculator
    matrix calculator

---
+++ matrix_calculations/main.py
@@ -0,0 +1,121 @@
+# matrix calculator
+# nov 2 - nov 15, 2018
+# by Eric Li
+# source: Ms. Healey, Jerry Wang
+# on my honor
+
+# this is a program that generates does two types of matrix calculations:
+#	1) reduced row echelon form (rref) calculation
+#	2) inverse matrix calculation 
+
+# terms explained:
+#	1) the size of a matrix with m rows and n columns is said to be of size m*n
+#	2) identity matrix (I): the diagnal matrix whose nonezero entries are all 1
+#	3) the rref:
+#		1. all nonzero rows (rows with at least one nonzero element) are above any rows of all zeroes (all zero rows, if any, belong at the bottom of the matrix)
+#		2. the leading coefficient (the first nonzero number from the left, also called the pivot) of a nonzero row is always strictly to the right of the leading coefficient of the row above it.
+#		3. the leading entry in each nonzero row is a 1 (called a leading 1).
+#		4. each column containing a leading 1 has zeros everywhere else.
+#	4) the inverse:
+#		the inverse of matrix A, denoted A**-1 is the matrix such that A*(A**-1/) = I = (A**-1)*A
+#	
+# inverse calcualtion explained:
+#	one of the many ways to get the inverse of the invertible matrix A is that:
+#		we augment I to to A
+#		do all row operations that lead A to its rref form (in this case because A is invertible rref(A)=I) to the augmented I as well
+#		when A is reduced to its rref form, the agumented matrix I becomes A's inverse
+
+# the program askes the user to generate a matrix based on:
+#	1) the size of the matrix
+#	2) each entry in the matrixautomatically
+# the program askes the user to choose between rref and inverse
+# based on the user's choice, the program calculates the corresponding matrix
+# the program prints out the rref form of the matrix in a nice grid
+
+# link file
+from theMatrix import Matrix
+
+# the function creates a grid for the rref matrix
+def print_matrix(mat):
+	c=-1
+	for entry in mat[0]:
+		c+=len(str(entry))+1
+
+	print(' '+'-'*c+' ')			# add horizontal lines at the top
+	for row in mat:
+		print('|',end='')			# add vertical lines on the left
+		for entry in row:
+			print(entry,end='')
+			print('|',end='')		# add vertical lines in the middle and on the right
+		print()
+		print(' '+'-'*c+' ')		# add horizontal line in the middle and on the bottom
+
+
+# the function askes for the user's input and has error checking abilities
+def entry():
+	global the_matrix
+	while True:
+		try:
+			print("Please enter the dimension of the matrix: \n(for instance, if the matrix is a 2 by 5 matrix, enter 2*5)")
+			dimension_raw = input()
+			dimension = dimension_raw.split('*')
+			if len(dimension) == 2:				# check to see if it is a 2D matrix
+				dimension[0] = int(dimension[0])	# number of rows
+				dimension[1] = int(dimension[1])	# number of columns
+				break
+			else:
+				print("please enter a valid dimension connected by '*'")
+			break
+
+		except ValueError:
+			print("Please enter a valid dimension connected by '*'")
+
+	rows= []
+	matrix_list = [[0 for i in range(dimension[1])]for i in range(dimension[0])]			# generate a list version of the matrix first
+	for i in range(dimension[0]):			# for each row in the matrix
+			position = "row "+str(i+1)
+			rows.append(position)			# store the position for it to be printed out
+			print("Please enter the values of the entries in "  + position+ " (separated by space): ")
+			while True:
+				try:
+					while True:
+						rowi = input()
+						rowi = rowi.split()			# allows for the program to take in multiple entries at a time
+						if len(rowi) == dimension[1]:	#check to see if the entries correspond with the size of the matrix
+							matrix_list[i] = rowi
+							break
+						else:
+							print("Please create a matrix corresponding to column number " + str(dimension[1])+".")
+
+					for j in range(dimension[1]):
+						matrix_list[i][j] = float(matrix_list[i][j])			# turn all strings into floats for further calculation
+					break
+
+				except ValueError:			#check to see if all entries are valid
+					print("Please enter entries that are real numbers.")
+
+	the_matrix = Matrix(matrix_list,dimension[0],dimension[1])			# generate the matrix in the class defined in the separate file
+
+
+#the function asks the user for hte type fo calculation and prints out the result
+def output():
+	print("Would you like to find:\n    1)The rref\n    2)The inverse")				# asks the user for the type of calculation
+	while True:
+		user_choice = input()
+		if user_choice == "1" or user_choice == "2":
+			print("The result is: ")
+			if user_choice == "1":
+				print_matrix(the_matrix.calc("rref"))			# print the rref form with the grid
+			if user_choice == "2":
+				# we are not printing the inverse in a grid because each entry in the inverse typically has numerous digits
+				# so it doesn't look good with the grid
+				print(the_matrix.calc("inverse"))			
+			break
+		else:			# error checking
+			print("Please enter 1 or 2 to indicate you choice.")
+
+#run the runctions
+entry()
+output()
+
+
---
+++ matrix_calculations/theMatrix.py
@@ -0,0 +1,181 @@
+# in order to use np.array()
+import numpy as np
+
+# create a class so that multiple matrices may use the same calucaltion
+class Matrix:
+	# constructor
+	def __init__(self, main_matrix, number_rows, number_columns):
+		self.matrix = np.array(main_matrix)			# turn the list version of the matrix in to an array
+		self.matrix_reference = np.array(main_matrix).copy()		# create a reference so that the matrix may be reset
+		# set up values for further calculations
+		self.nonzero_rows = number_rows		# this value indicates the number of rows with at lease one nonzero entry
+		self.total_row_swapped = 0			# this value indicates how many rows are swapped during the calculation
+		self.number_columns = number_columns		# size of the matrix
+		self.number_rows = number_rows				# size of the matrix
+		self.identity = [[0 for i in range(number_rows)] for i in range(number_columns)]
+		for i in range(number_rows):
+			self.identity[i][i] = 1
+		self.identity = np.array(self.identity)			# create an identity matrix for the potential inverse calculation
+
+	# the method detects if there's a zero row in the matrix and moves it to the bottom of the matrix
+	def check_zero_rows(self):
+		zero_count = 0;
+		count = 0
+		while count < self.nonzero_rows:			# go through every row that are now labeled as nonzero
+			if np.all(self.matrix[count] == 0) == True:			# if the row is turned into a zero row by the previous operation
+				row_moved = self.matrix[count]
+				self.matrix = np.delete(self.matrix, (count), axis = 0)			# removed the zero row
+				zero_count += 1				# number of rows removed
+				self.nonzero_rows -= 1		# one fewer row labeled as nonzero
+				self.total_row_swapped += 1			# now in total there's one row swapped
+			else:
+				count += 1
+		# add the same number of zero rows as the number of removed rows to the bottom of the array
+		for i in range(zero_count):
+			self.matrix = np.append(self.matrix, [row_moved], axis = 0)
+
+	# the method returns the number of entries in each row starting from each pivot point
+	def len_check(self, row):
+		row_check = self.matrix[row].copy()			# make a copy so that the original matrix is not changed
+		column_remaining = self.number_columns		# how many entries are left
+
+		j = 0			# starting from the first entry in each row
+
+		while j < column_remaining:
+			if row_check[j] != 0:		# if this entry is nonzero	
+				break	
+			elif row_check[j] == 0:			# if this entry is zero	
+				row_check = np.delete(row_check, j)		# removed the zero entry
+				column_remaining -= 1			# how many entries are left; this makes sure that the while loop doesn't run beyond the length of the row that's left
+		return len(row_check)			# return the length of the row that's left
+
+	# the method, depending on the length of each row defined in len_check(), makes sure that all more lengthy rows are higher up than less lengthy rows
+	# this helps makes sure that each column containing a leading nonzero entry has zeros everywhere else.
+	def check_length_rows(self):
+		rows_swapped = 0
+		row_length = []
+		for i in range(self.nonzero_rows):
+			row_length.append(self.len_check(i))		# use a list to store all valeus for further cross comparisons
+		# print(self.identity)
+		i = 0
+		while i < (self.nonzero_rows-1):		# the program goes through all rows to make sure that the descending order is consistent throughout the matrix
+			if row_length[i+1] > row_length[i]:		# if the row higher up is longer than the row beneath
+				row_length[i], row_length[i+1] = row_length[i+1], row_length[i]			# stor the swapped row lengths
+				self.matrix[[i,i+1]] = self.matrix[[i+1,i]] 			# swap rows
+
+				rows_swapped += 1
+			i += 1
+
+		# because each time when the function is run there's at most one row that is moved
+		# no matter how many times that row is moved down, the total_row_swapped value only increases by one
+		if rows_swapped != 0:			
+			self.total_row_swapped += 1			
+
+	# this method runs through both the rref calculation and the inverse calculation
+	# no matter what the user_choice is, th method runs through the rref calculation first
+	# because we first need to know, through the rref value, whether the matrix is invertible
+	# for inverse calculation, once we know that the matrix is invertible, we run through the method again, this time indicating that we're doing inverse calculations
+	# when the method is run for the inverse calculation it runs the matrix, denoted A, along with the augmented identity matrix,
+	# so that whatever row operation is done to A it is also done to the identity matrix
+	# when A becomes I, the augmented I becomes A inverse
+	def calc(self, calc_type):			# calc_type indicates whether this is for rref calcualtions or inverse calcualtions
+
+		i = 1
+		m = self.number_rows			# for concision purposes
+		n = self.number_columns
+
+
+		# check if there's zero rows and if the length of the rows are in a descending order
+		self.check_zero_rows()
+		self.check_length_rows()
+
+		while i < self.nonzero_rows:			# for all nonzero rows
+
+			# if the two adjacent rows have the same length
+			# cancel out the pivot value of the lower row by subtracting a multiple of the higher row from it
+			if self.len_check(i) == self.len_check(i-1):
+				# calculate the ratio between the pivots of the two rows			
+				if calc_type == "inverse_calc":			# if this method is run in order to calculate the inverse					
+					# the pivot is in position 2*n-self.len_check(i)
+					# we need n*2 because the augmented matrix doubles the width of the matrix
+					coef = (self.matrix[i][2*n-self.len_check(i)]/self.matrix[i-1][2*n-self.len_check(i)])			
+				else:		# if this method is run in order to calculate the rref
+					# the pivot is in position n-self.len_check(i)
+					coef = (self.matrix[i][n-self.len_check(i)]/self.matrix[i-1][n-self.len_check(i)])
+				self.matrix[i] = self.matrix[i] - (coef*self.matrix[i-1])			# subtract a multiple of the higher row from the lower row
+				
+				# check if there's zero rows and if the length of the rows are in a descending order
+				self.check_zero_rows()
+				self.check_length_rows()
+			
+			# if the two adjacent rows do not have the same length
+			# move on to the next row
+			else:
+				i += 1
+
+		# cancel out all nonzero values after the pivot by subtracting multiples of all other shorter rows from that row
+		for i in range(self.nonzero_rows):			# for all nonzero rows, each row denoted row(A)
+			for j in range(i+1, self.nonzero_rows):		# for all rows beneath the row(A) (that are shorter in length)
+				for z in range(1,n):						# a constant that runs through all potential number of entries
+					if self.len_check(i) == (self.len_check(j) + z):	# for all rows shoter than row(A)
+						# calculate the ratio between the two entries in the column correspongding to the pivot column of the shorter row
+						if calc_type == "inverse_calc":			# if this method is run in order to calculate the inverse
+							# the column is in position 2*n+z-self.len_check(i)]/self.matrix[j]
+							# we need n*2 because the augmented matrix doubles the width of the matrix
+							coef = (self.matrix[i][2*n+z-self.len_check(i)]/self.matrix[j][2*n+z-self.len_check(i)])
+						else:	# if this method is run in order to calculate the rref
+							# the column is in position n+z-self.len_check(i)]/self.matrix[j]
+							coef = (self.matrix[i][n+z-self.len_check(i)]/self.matrix[j][n+z-self.len_check(i)])
+
+						self.matrix[i] = self.matrix[i] - (coef*self.matrix[j])			# subtract a multiple of the shorter row from the longer row
+					
+		# now all that's left are nonzero pivots
+		# divide each row by its pivot value so that all leading nonzero entries become 1
+		for i in range(self.nonzero_rows):			#for all nonzero rows
+			if calc_type == "inverse_calc":
+				# the pivot is in position 2*n-self.len_check(i)
+				self.matrix[i] = self.matrix[i]/(self.matrix[i][2*n-self.len_check(i)])
+			else:
+				# the pivot is in position n-self.len_check(i)
+				self.matrix[i] = self.matrix[i]/(self.matrix[i][n-self.len_check(i)])
+
+		# for all values, round the answers so that the decimals do not get too lengthy
+		for i in range(m):
+			for j in range(n):
+				self.matrix[i][j] = round(self.matrix[i][j],3)
+				# because array calcualtions leave -0 as -0, we convert it to 0 instead
+				if self.matrix[i][j] == -0:
+					self.matrix[i][j] = 0
+					
+		# return the result if the user chooses the rref calculation
+		if calc_type == "rref":
+			return self.matrix
+		
+		# if the user wchooses the inverse calculation
+		elif calc_type == "inverse":
+			# scenarios where the matrix is not invertible
+			if m != n:				# if the matrix is not a square matrix
+				return "The matrix is not invertible."
+			
+			else:
+				one_count = 0
+				for i in range(m):
+					if self.matrix[i][i] == 1:		# check through the diagnal entries to see if each entry is 1
+						one_count += 1
+				if one_count != m:			# if not all diagnal entries are 1 (if the rref of the matrix is not I)
+					return "The matrix is not invertible."
+
+				else:				# if the rref is I, the matrix is invertible
+					# augment the identity matrix to the matrix
+					augmented=[]
+					for x,row in enumerate(self.matrix_reference):
+						augmented.append(np.array(list(row)+list(self.identity[x])))
+					self.matrix=np.array(augmented)
+
+					# run through the function again, this time doing the inverse calculations with the indentity matrix augmented
+					return self.calc("inverse_calc") 
+		
+		# if it is indicated that the method is doing inverse_calc
+		else:
+			return self.matrix[:m,m:]			# return only the augmented matrix
+
